aa110864d12274e245a4ce9b2a0b7f1c0dccd55c
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-03 11:39:52 +0500

    Add project files.

---
+++ Arrays 2.2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        Console.Write("Enter rows length : ");
+        int row = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter columns length : ");
+        int column = Convert.ToInt32(Console.ReadLine());
+        int[,] arr = new int[row, column];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                arr[i, j] = random.Next(1, 10);
+                Console.Write(arr[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        int k;
+        Console.Write("Enter k number : ");
+        k = Convert.ToInt32(Console.ReadLine());
+
+        int summa = 0;
+        int multiplication = 1;
+
+        for (int j = 0; j < column; j++)
+        {
+            summa += arr[j, k];
+            multiplication *= arr[j, k];
+        }
+
+        Console.WriteLine($"Summa : {summa}");
+        Console.WriteLine($"Multiplication : {multiplication}");
+    }
+}
